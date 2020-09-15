58d580a5f2fa875afda6bb163c075d9acea7d1bf
EdaDuan <308506994@qq.com>
2020-09-15 08:10:33 +0800

    wrote test three

---
+++ src/ScrollLoadSimple/Com/Test.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './style3.css';
+class Test extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="about-wrapper">
+        <div className="about-text">
+          <div className={this.props.className}>
+            <h3>Test GIt</h3>
+            <p>This is a Test that will appear.</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Test;
